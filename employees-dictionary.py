9bc7e8ff7832ccafae2e49fc3a01de724418cfec
MoutazObeid <moutazobeid24@gmail.com>
2025-03-01 22:18:56 +0200

    Added the Master dictionary and putted the slaves dictonries within it

---
+++ employees-dictionary.py
@@ -0,0 +1,21 @@
+
+company_employees ={
+    "engineering" :{
+        "alice" : {
+            "age":30,
+            "role": "Software Engineer"
+        },
+         "bob": {
+            "age":28,
+            "role": "DevOps Engineer"
+        }
+    },
+    "HR" :{
+    "Charlie":{
+    "age":35,
+    "role": "HR Manager"
+                }
+
+    }
+}
+print(company_employees)
