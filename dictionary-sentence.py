4c4c8d4e319c713c9df9a05d8b9b423a895d401e
MoutazObeid <moutazobeid24@gmail.com>
2025-03-01 21:34:19 +0200

    Finalized the code to add and count the world into the dictionary

--- dictionary-sentence.py
+++ dictionary-sentence.py
@@ -1,5 +1,13 @@
 sentence = input("enter a sentence please: ")
 
-word-count = {}
+word_count = {}
 
 list_words = sentence.split()
+
+for i in list_words:
+    if i in word_count:
+        word_count[i] +=1
+
+    else:
+        word_count[i] = 1    
+print(word_count)
